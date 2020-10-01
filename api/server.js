8c36091924826f07413fc930060cedc767acf05e
roboblox <supeecyborg@gmail.com>
2020-10-01 13:35:05 -0400

    working

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express"); // CommonJS modules
+const server = express();
+// server.use(express.json()); // remember to invoke json()
+
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "Hello" });
+});
+
+module.exports = server;
+// const express = require("express");
+// const server = express();
+// server.get("/", (req, res) => {
+//     res.status(200).json({ hello: "node 34" });
+// });
+// module.exports = server;
